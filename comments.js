ce3a809117b20798cbb20567bb6b81f88bd37498
lenaabedali <137562356+lenaabedali@users.noreply.github.com>
2025-03-24 14:21:05 +0000

    3rd Copilot Commit (Round 3)

--- comments.js
+++ comments.js
@@ -1,43 +1,55 @@
 // create web server
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
-const qs = require('querystring');
-const comments = [];
-http.createServer((req, res) => {
-    const { pathname } = url.parse(req.url);
-    if (req.method === 'GET') {
-        if (pathname === '/') {
-            fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-                if (err) {
-                    res.end('Server error');
-                    return;
-                }
-                res.end(data);
-            });
-        } else if (pathname === '/comments') {
-            res.end(JSON.stringify(comments));
-        } else {
-            res.end('404');
-        }
-    } else if (req.method === 'POST') {
-        if (pathname === '/comment') {
-            let data = '';
-            req.on('data', chunk => {
-                data += chunk;
-            });
-            req.on('end', () => {
-                const comment = qs.parse(data);
-                comments.push(comment);
-                res.end('success');
-            });
-        } else {
-            res.end('404');
-        }
-    } else {
-        res.end('404');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+
+// set up body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set up static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up comments file
+var commentsFile = path.join(__dirname, 'data', 'comments.json');
+
+// get comments
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error reading comments file');
+      return;
+    }     
+    res.json(JSON.parse(data));
+  });
+});
+
+// add comment
+app.post('/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error reading comments file');
+      return;
     }
-}).listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile(commentsFile, JSON.stringify(comments), function(err) {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error writing comments file');
+        return;
+      }
+      res.json(comments);
+    });
+  });
+});
+
+// start server
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
 });
